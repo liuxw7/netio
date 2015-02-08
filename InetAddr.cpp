7c78e355a10855db337018b215e1acdde1400dc4
liuzf <liuzf1986@163.com>
2015-02-08 22:35:12 +0800

    test InetAddr class

--- InetAddr.cpp
+++ InetAddr.cpp
@@ -1,6 +1,7 @@
 
 #include <strings.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 
 #include "InetAddr.hpp"
 #include "Endian.hpp"
@@ -13,7 +14,7 @@ InetAddr::InetAddr(std::string ip, uint16_t port) {
   bzero(&_sockaddr, sizeof(struct sockaddr_in));
 
   _sockaddr.sin_family = AF_INET;
-  _sockaddr.sin_port = Endian::hton16(port());
+  _sockaddr.sin_port = Endian::hton16(port);
   inet_pton(AF_INET, ip.c_str(), &_sockaddr.sin_addr);
 }
 
@@ -30,54 +31,55 @@ InetAddr::InetAddr(uint16_t port) {
 
   _sockaddr.sin_family = AF_INET;
   _sockaddr.sin_addr.s_addr = INADDR_ANY;
-  _sockaddr.sin_port = Endian::hton16(port());
+  _sockaddr.sin_port = Endian::hton16(port);
 }
   
 std::string InetAddr::strIp() const {
-  inet_ntop(int __af, const void *restrict __cp, char *restrict __buf, socklen_t __len)
+  char ipstr[16] = {0};
+  inet_ntop(AF_INET, (void*)&_sockaddr.sin_addr, ipstr, sizeof(ipstr));
+  return ipstr;
 }
 
 std::string InetAddr::strIpPort() const {
-  
+  char ipstr[16] = {0};
+  char ipportstr[22] = {0};
+
+  inet_ntop(AF_INET, (void*)&_sockaddr.sin_addr, ipstr, sizeof(ipstr));
+  snprintf(ipportstr, sizeof(ipportstr), "%s:%u", ipstr, Endian::ntoh16(_sockaddr.sin_port));
+  return ipportstr;
 }
 
 uint32_t InetAddr::ip() const {
-  
+  return Endian::ntoh32(_sockaddr.sin_addr.s_addr);
 }
 uint16_t InetAddr::port() const {
-  
+  return Endian::ntoh16(_sockaddr.sin_port);
 }
 
 uint32_t InetAddr::netEndianIp() const {
-  
+  return _sockaddr.sin_addr.s_addr;
 }
 
 uint32_t InetAddr::netEndianPort() const {
-  
+  return _sockaddr.sin_port;
 }
+
+static __thread char __resolve_buf[8 * 1024];
 
 bool InetAddr::resolve(const std::string& host, InetAddr& addr) {
+  struct hostent ent;
+  struct hostent *pent = nullptr;
+  int henterr = 0;
   
+  bzero(&ent, sizeof(struct hostent));
+
+  int ret = gethostbyname_r(host.c_str(), &ent, __resolve_buf, sizeof(__resolve_buf), &pent, &henterr);
+  if(0 == ret && pent != nullptr) {
+    addr._sockaddr.sin_addr = *reinterpret_cast<struct in_addr*>(pent->h_addr);
+    return true;
+  } else {
+    return false;
+  }
 }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ test.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include "InetAddr.hpp"
+
+using namespace netio;
+
+int main(int argc, char *argv[])
+{
+  InetAddr addr(16);
+  std::cout << "addr : " << addr.strIpPort() << std::endl;
+
+  InetAddr addr2(0);
+
+  bool resolved = InetAddr::resolve("www.baidu.com", addr2);
+
+  std::cout << "baidu resolved " << resolved << ", addr= " << addr2.strIp() << std::endl;
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
