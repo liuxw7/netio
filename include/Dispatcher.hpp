0b4f8a6b745e109fb04a0254065382fa59252a19
liuzf1986 <liuzf1986@163.com>
2015-03-19 00:08:20 +0800

    modify proxy

--- include/Dispatcher.hpp
+++ include/Dispatcher.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <list>
 #include <functional>
 #include <mutex>
 #include <strings.h>
@@ -21,19 +22,31 @@ template <typename KEYTYPE>
 class Dispatcher : public Noncopyable {
   typedef function<void(SpVecBuffer&, PMAddr&)> Handler;
  public:
+  // handler for all events 
+  void registerAnyHandler(Handler& callback) {
+    _anyHandles.push_back(callback);
+  }
+  void registerAnyHandler(Handler&& callback) {
+    _anyHandles.push_back(std::move(callback));
+  }
   // register handlers
   void registerHandler(KEYTYPE& key, Handler& callback) {
-    unique_lock<mutex> lock(_mutex);
     _handleMap.insert(std::pair<KEYTYPE, Handler>(key, callback));
   }
   void registerHandler(KEYTYPE& key, Handler&& callback) {
-    unique_lock<mutex> lock(_mutex);
     _handleMap.insert(std::pair<KEYTYPE, Handler>(key, std::move(callback)));
   }
 
   // dispatch buffer
   void dispatch(const KEYTYPE& key, SpVecBuffer& buffer, PMAddr& peerInfo) {
     //    map<KEYTYPE, Handler>::const_iterator iter = _handleMap.find(key);
+
+    auto anyIter = _anyHandles.begin();
+    while(anyIter != _anyHandles.end()) {
+      (*anyIter)(buffer, peerInfo);
+      anyIter ++;
+    }
+    
     auto iter = _handleMap.find(key);
 
     if(LIKELY(iter != _handleMap.end())) {
@@ -61,11 +74,21 @@ class Dispatcher : public Noncopyable {
   // }
 
   map<KEYTYPE, Handler> _handleMap;
-  mutable mutex _mutex;
+  list<Handler> _anyHandles;
 
   static __thread char _dbgKeyInfo[50];
 };
 
 }
+
+
+
+
+
+
+
+
+
+
 
 
--- include/TcpProxy.hpp
+++ include/TcpProxy.hpp
@@ -1,16 +1,59 @@
 #pragma once
 
+#include <map>
+#include <time.h>
+#include <memory>
+#include <functional>
+
 #include "TcpServer.hpp"
+#include "TcpConnection.hpp"
 #include "Dispatcher.hpp"
 
 namespace netio {
 
+class UserSession {
+  typedef shared_ptr<TcpConnection> SpTcpConnection;
+ public:
+  UserSession(int id, time_t createTime, SpTcpConnection connection) :
+      _id(id),
+      _createTime(createTime),
+      _updateTime(createTime),
+      _connection(connection),
+      _seq(0)
+  {}
+
+  void touch(time_t updateTime) { _updateTime = updateTime; }
+
+  uint32_t getId() const { return _id; }
+
+  time_t createTime() const { return _createTime; }
+
+  time_t updateTime() const { return _updateTime; }
+
+  
+ private:
+  uint32_t _id; // session id is unique global for distribute system.
+  time_t _createTime;
+  time_t _updateTime;
+  uint32_t _seq; // sequence number for generate push message sequence number.
+  SpTcpConnection _connection;
+};
+
+class UserSessionMgr {
+ public:
+  void process(SpVecBuffer& buf, PMAddr& addr) {
+    
+  }
+ private:
+  map<int, UpUserSession> _usMap;
+};
+
 /**
  * 
  */
-template <class CMDTYPE>
 class TcpProxy {
   typedef shared_ptr<LooperPool<MultiplexLooper> > SpLooperPool;
+  typedef unique_ptr<UserSession> UpUserSession;
  public:
   /**
    * @loopsers hold threads for process events.
@@ -19,18 +62,23 @@ class TcpProxy {
    */
   explicit TcpProxy(SpLooperPool& loopers, uint16_t servPort, uint16_t pushPort);
 
-  /**
-   * 
-   */
-  
-  
   
   Dispatcher& getDispatcher() { return _dispatcher; }
  private:
   
+  SpLooperPool _loopPool;
+  UserSessionMgr _usMgr;
   TcpServer _server;
-  Dispatcher<CMDTYPE> _dispatcher;
+  Dispatcher<int> _dispatcher;
 };
 
+TcpProxy::TcpProxy(SpLooperPool& loopers, uint16_t servPort, uint16_t pushPort) :
+    _loopPool(loopers),
+    _server(servPort, loopers)
+{
+  _dispatcher.registerAnyHandler(std::bind(&UserSessionMgr::process, &_usMgr, std::placeholders::_1, std::placeholders::_2));
 }
+
+}
+
 
