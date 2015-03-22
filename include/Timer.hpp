2fafb0185ff69d6a46c31919a5378b3197ab8852
liuzf1986 <liuzf1986@163.com>
2015-03-23 00:43:08 +0800

    add Timer header

---
+++ include/Timer.hpp
@@ -0,0 +1,32 @@
+
+#include <functional>
+
+namespace netio {
+
+class TimerEntry {
+  
+};
+
+class SimpleWheelTimer {
+  
+};
+
+class HierarchyWheelTimer {
+  
+};
+
+class HeapTimer {
+  
+};
+
+}
+
+
+
+
+
+
+
+
+
+
